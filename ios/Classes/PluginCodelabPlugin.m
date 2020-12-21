d1ff30b6972d68e191a5189d080ae7c5a3965963
Samuel <samuelbeebest@gmail.com>
2020-12-21 16:33:16 +0100

    first commit

---
+++ ios/Classes/PluginCodelabPlugin.m
@@ -0,0 +1,20 @@
+#import "PluginCodelabPlugin.h"
+
+@implementation PluginCodelabPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"plugin_codelab"
+            binaryMessenger:[registrar messenger]];
+  PluginCodelabPlugin* instance = [[PluginCodelabPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
